def5e1aa9cc1408fcabeeaa4e8ec907b78b6cba7
L.I.Takumi <32321945+hineya09@users.noreply.github.com>
2024-01-25 22:30:21 +0900

    first commit

---
+++ src/background.js
@@ -0,0 +1,9 @@
+﻿chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && tab.url.indexOf("atlassian.net/wiki/") > -1) {
+    console.log(`updated: ${tab.url}`);
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        files: ['./jquery-2.2.0.min.js', './content_script.js'],
+    });
+  }
+});
---
+++ src/content_script.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    // ページプロパティマクロの列幅を揃える
+    $('table.metadata-summary-macro tr').find('th:nth-of-type(1), td:nth-of-type(1)').css('width','30%');
+    $('table.metadata-summary-macro tr').find('th:nth-of-type(2), td:nth-of-type(2)').css('width','50%');
+    $('table.metadata-summary-macro tr').find('th:nth-of-type(3), td:nth-of-type(3)').css('width','20%');
+
+    // ラベルの省略表示をやめる
+    $('table.metadata-summary-macro .pageLabel').css('max-width', 'none');
+});
